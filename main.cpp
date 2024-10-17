ee356bdaf69d57a9ec1a1dcf5f245be8790c8017
Bartlomiej Dlugosz <bd523@ic.ac.uk>
2024-10-17 22:24:40 +0100

    Feat: Implemented Numerical encoding for lists

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 class SingleNumCoding {
 private:
@@ -56,6 +57,44 @@ public:
 
   int getX() { return x; }
   int getY() { return y; }
+};
+
+class ListNumCoding {
+private:
+  std::vector<int> nums;
+public:
+  ListNumCoding(std::vector<int> nums) {
+    this->nums = nums;
+  }
+
+  ListNumCoding(int num) {
+    while (num > 0) {
+      int n = 0;
+      while ((num & 0b1) == 0) {
+        num = num >> 1;
+        n++;
+      }
+      nums.push_back(n);
+      num = num >> 1;
+    }
+  }
+
+  int num() {
+    int res = 0;
+    // res = 1 << nums[0];
+    // res = 1 << nums[1] << nums[0];
+    // res = 1 << nums[2] << nums[1] << nums[0];
+    for (int i = 0; i < nums.size(); i++) {
+      int new_num = 1 << nums[i];
+      for (int j = 0; j < i; j++) {
+        new_num = new_num << (nums[j] + 1);
+      }
+      res += new_num;
+    }
+    return res;
+  }
+
+  std::vector<int> getNums() { return nums; }
 };
 
 int main() {
